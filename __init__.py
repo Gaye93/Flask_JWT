b406a1b61b289eedd50108ab971c5b33c2a16de5
Gaye Camara <122286715+Gaye93@users.noreply.github.com>
2025-05-09 16:42:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,12 +27,13 @@ def hello_world():
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
+    
     if username != "test" or password != "test":
         return jsonify({"msg": "Mauvais utilisateur ou mot de passe"}), 401
 
-    access_token = create_access_token(identity=username)
+    # Ajout du rôle dans le token
+    access_token = create_access_token(identity={"username": username, "role": "admin"})
     return jsonify(access_token=access_token)
-
 
 # Route protégée par un jeton valide
 @app.route("/protected", methods=["GET"])
@@ -40,6 +41,15 @@ def login():
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+
+@app.route("/admin", methods=["GET"])
+@jwt_required()
+def admin():
+    identity = get_jwt_identity()
+    if identity.get("role") != "admin":
+        return jsonify({"msg": "Accès refusé : vous n'êtes pas admin"}), 403
+
+    return jsonify({"msg": f"Bienvenue Admin {identity['username']} !"}), 200
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
